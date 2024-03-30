be9806200a680771f0c9052128288c04a7bfb99c
carneirotg <carneirotg@gmail.com>
2024-03-30 15:41:14 +0100

    Initial commit

---
+++ src/main/java/com/phonebase/phonebase/PhonebaseApplication.java
@@ -0,0 +1,13 @@
+package com.phonebase.phonebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebaseApplication.class, args);
+	}
+
+}
